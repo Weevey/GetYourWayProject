9237735edbe312029502c93d5ab8de3c02a44130
Steve Ayres <115733238+Weevey@users.noreply.github.com>
2023-02-13 19:25:36 +0000

    Adding initial Springboot files

---
+++ src/test/java/com/sky/getYourWayLGs/GetYourWayLGsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.getYourWayLGs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetYourWayLGsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
